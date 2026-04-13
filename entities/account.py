4dbd01c4482b2fe1f1cde28898c9b8c9ff2b1ae8
luiskevinms <luiskevinms@gmail.com>
2026-04-13 15:19:19 -0700

    Actualizacion de hoy, se agregó account.py, transaction.py y el tipo de transaction en enums

---
+++ entities/account.py
@@ -0,0 +1,34 @@
+from datetime import datetime
+from entities.user import User
+from entities.transaction import Transaction
+from persistence.db import get_connection
+import pymysql
+
+class Account():
+
+    def __init__(self, id: int, number: str, creation_date: datetime, user: User, transactions: list):
+        self.id = id
+        self.number = number
+        self.creation_date = creation_date
+        self.user = user
+        self.transactions = transactions
+
+    def get_account_by_id(id_user: int):
+        try:
+            connection = get_connection()
+            cursor = connection.cursor(pymysql.cursors.DictCursor)
+
+            sql = "SELECT id, number, creation_date, id_user FROM account WHERE id_user = %s"
+            cursor.execute(sql, (id_user,))
+
+            rs = cursor.fetchone()
+
+            user = User.get_by_id(rs["id_user"])
+            transactions = Transaction.get_transactions_by_account(rs["id"])
+            account = Account(
+                rs["id"],
+                rs["number"],
+                rs["creation_date"],
+                user,
+                transactions
+            )
---
+++ enums/transaction_type.py
@@ -0,0 +1,7 @@
+from enum import Enum
+
+class TransactionType(Enum):
+    INCOME = 1
+    EXPENSE = 2
+
+#Hacer un metodo transaction get_transactions_by_account(id_account: int)
